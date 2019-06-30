fec71a840c384a20f09079df3e40de247240e18b
Pravin Vasudevan <vv.pravin@gmail.com>
2019-06-29 23:15:12 -0700

    fixing checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
   * @param string someone
   * @return formatted greeting 
   */
-  public String final greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
